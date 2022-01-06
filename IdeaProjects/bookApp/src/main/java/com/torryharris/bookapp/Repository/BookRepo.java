eab9582df5c6f31c36f292486c2fcd862eaf2328
AshwinThonoor <89892003+AshwinThonoor@users.noreply.github.com>
2022-01-06 11:54:31 +0530

    Delete IdeaProjects/bookApp directory

--- IdeaProjects/bookApp/src/main/java/com/torryharris/bookapp/Repository/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.torryharris.bookapp.Repository;
-
-import com.torryharris.bookapp.Model.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Books,String> {
-}
--- IdeaProjects/bookApp/src/main/java/com/torryharris/bookapp/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.torryharris.bookapp.Repository;
-
-import com.torryharris.bookapp.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,String> {
-}
